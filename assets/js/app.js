3b20bcf677325a01ea506dd4415940c73c46a104
jojobeth1@gmail.com <jojobeth1@gmail.com>
2017-06-30 11:32:00 -0700

    added favicon. Modified CSS code. added content to html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,4 @@
 // sanity check
 console.log("app.js linked.");
+//in the future, we can add jquery event listeners here, like for hover :)
+//make sure to put it in the document.ready so it loads appropriately
